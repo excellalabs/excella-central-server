b305dee2a3a7ddad7147f0ab0d3fe645e0c67695
Sam Blissard <samblissard@gmail.com>
2017-12-05 19:41:48 -0500

    Initital verification email

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,27 @@
 'use strict';
+var config = require('../../server/config.json');
+var path = require('path');
 
 module.exports = function(Account) {
-
-};
+    Account.afterRemote('create', function(context, userInstance, next) {
+        console.log('> user.afterRemote triggered');
+    
+        var options = {
+          type: 'email',
+          to: userInstance.email,
+          from: 'central@excella.com',
+          subject: 'Thanks for registering.',
+          template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+          redirect: '/verified',
+          user: Account
+        };
+    
+        userInstance.verify(options, function(err, response, next) {
+            if (err) return next(err);
+            
+            console.log('> verification email sent:', response);
+    
+            return response;
+        });
+      });
+}
