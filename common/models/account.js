3caa0d9fb9fd95d3edf9c0efe037ea7e6139d04e
Brendan Sawyer <brendan.sawyer@excella.com>
2017-12-09 16:10:44 -0500

    Added new API to checkAccountExists for

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Account) {
+  Account.checkAccountExists = function(email, cb) {
+    if (!email || email === '') {
+      return cb(null, false);
+    }
+    Account.find({where: {email: email}}, function(err, instance) {
+      var accountExists = instance && instance.length > 0;
+      return cb(null, accountExists);
+    });
+  };
 
+  Account.remoteMethod (
+    'checkAccountExists',
+    {
+      http: { path: '/checkAccountExists', verb: 'get' },
+      accepts: { arg: 'email', type: 'string', http: { source: 'query' } },
+      returns: { arg: 'doesAccountExist', type: 'boolean'}
+    }
+  );
 };
