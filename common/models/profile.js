325ee34c93d0b2c6a79e40639642859f9d7484f9
Brendan Sawyer <brendan.sawyer@excella.com>
2017-12-13 23:42:09 -0500

    Fix return type

--- common/models/profile.js
+++ common/models/profile.js
@@ -18,7 +18,7 @@ module.exports = function(Profile) {
     {
       http: { path: '/search', verb: 'get' },
       accepts: { arg: 'searchText', type: 'string', http: { source: 'query' } },
-      returns: { arg: 'profiles', type: 'object'}
+      returns: { arg: 'profiles', type: 'array'}
     }
   );
 
