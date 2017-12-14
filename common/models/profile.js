519f984c90480c5a50fa8713d2d1e485e0db32e8
Brendan Sawyer <brendan.sawyer@excella.com>
2017-12-13 23:11:57 -0500

    Added profile "search" method

--- common/models/profile.js
+++ common/models/profile.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Profile) {
+  Profile.search = function(searchText, cb) {
+    if (!searchText || searchText === '') {
+      Profile.find({}, function(err, instance) {
+        return cb(null, instance);
+      });
+    } else {
+      searchText = '/' + searchText + '/';
+      Profile.find({where: {$text: {search: searchText}}}, function(err, instance) {
+        console.log(searchText);
+        return cb(null, instance);
+      });
+    }
+  };
+
+  Profile.remoteMethod (
+    'search',
+    {
+      http: { path: '/search', verb: 'get' },
+      accepts: { arg: 'searchText', type: 'string', http: { source: 'query' } },
+      returns: { arg: 'profiles', type: 'object'}
+    }
+  );
 
 };
