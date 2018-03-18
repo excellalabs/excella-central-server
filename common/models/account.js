d049d6984b67ad99bb368e076f7a8484d32f523e
samblissard <samblissard@gmail.com>
2018-03-18 14:45:32 -0400

    Sending verification email with link

--- common/models/account.js
+++ common/models/account.js
@@ -1,4 +1,6 @@
 'use strict';
+var config = require('../../server/config.json');
+var path = require('path');
 module.exports = function(Account) {
   var app = require('../../server/server');
 
@@ -53,6 +55,28 @@ module.exports = function(Account) {
     }, function(err) {
       if (err) return console.log('> error sending password reset email');
       console.log('> sending password reset email to:', info.email);
+    });
+  });
+
+  Account.afterRemote('create', function(context, userInstance, next) {
+    console.log('> user.afterRemote triggered');
+
+    var options = {
+      type: 'email',
+      to: userInstance.email,
+      from: 'central@excella.com',
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: Account
+    };
+
+    userInstance.verify(options, function(err, response, next) {
+        if (err) return next(err);
+        
+        console.log('> verification email sent:', response);
+
+        return response;
     });
   });
 };
